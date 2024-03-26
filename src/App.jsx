d6cfb7b8b9927aceca8980cb37471bfd8e1345c6
Tolga <123948207+tolgapp@users.noreply.github.com>
2024-03-26 16:11:56 +0100

    pages comps added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
 import "./index.css";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar />
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/contact"} element={<Contact />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import skylines from "../../public/images/skylines.jpeg"
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <h1 className="absolute text-8xl text-white drop-shadow-lg top-1/2 inset-24">0 fees and more banking!</h1>
+      <div className="home-picture">
+        <img src={skylines} alt="skylines" />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
