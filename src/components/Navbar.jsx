d0b1817feedf8fdd46cda4b22232c4585351055e
Tolga <123948207+tolgapp@users.noreply.github.com>
2024-03-30 16:14:01 +0100

    started with service page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav
-      className="flex drop-shadow-md justify-between bg-white right-0 p-8 z-50 md:sticky md:top-0 md:left-0"
-    >
+    <nav className="flex drop-shadow-md justify-between bg-white right-0 p-8 z-50 md:sticky md:top-0 md:left-0">
       <section className="flex md:place-content-center md:m-4">
         <h1 className="text-3xl font-bold ml-2 md:text-5xl">noFee ▼</h1>
       </section>
@@ -12,7 +10,9 @@ const Navbar = () => {
         <Link to={"/"}>Home</Link>
         <Link to={"/plans"}>Plans</Link>
         <Link to={"/service"}>Service</Link>
-        <Link to={"/signup"} className="bg-stone-800 text-white p-3 rounded-xl">Sign Up</Link>
+        <Link to={"/signup"} className="bg-stone-800 text-white p-3 rounded-xl">
+          Sign Up
+        </Link>
       </ul>
     </nav>
   );
--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,16 +1,20 @@
+import service from "/images/service.png"
+
 const Service = () => {
   return (
     <>
-    <div className=''>
-      <div>
-      <h2>Maybe we can not help you with your Brunch but</h2>
-      <h3>if you have problem with your bank account</h3>
-      <h4>we can help you 24/7!</h4>
+    <div className='flex w-full'>
+      <div className="w-1/2 place-content-center pl-12 pr-12 font-semibold bg-stone-900">
+      <h2 className="text-8xl text-white">Travel ✈️, Party 🪩 or Chill 🛀🏻 and if you have problem with your bank account we are 24/7 here!</h2>
       </div>
-      <article className=''>
-        <img src="" alt="" />
+      <article className='w-1/2'>
+        <img className="" src={service} alt="24 / 7 Service" />
       </article>
     </div>
+    <form action="" className="h-screen flex flex-col">
+      <input type="text" name="name" id="" placeholder="Name" className="border p-3 w-1/2"/>
+      <input type="text" name="fullname" id="" placeholder="Fullname" className="border p-3 w-1/2"/>
+    </form>
     </>
   )
 }
