f752d76c35020ad0c28b1f70d4b62d1feabbdd6f
Tolga <123948207+tolgapp@users.noreply.github.com>
2024-03-30 21:17:59 +0100

    added info text for mail support

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -11,6 +11,9 @@ const Service = () => {
      <ServiceData image={service} text={`Travel ✈️, Party 🪩 or Chill 🛀🏻 and if you have problem with your
             bank account we are 24/7 here!`}/>
     <ServiceData image={mail} text={`Get the app for more contact options!`} next={next}/>
+    <div className="flex h-96 place-content-center items-center border">
+      <h4 className="text-7xl pl-20 pr-20 text-left">*We will anser via mail within 72 hours or you can call us on the number in the app if you have an urgent question.</h4>
+    </div>
     </>
   );
 };
